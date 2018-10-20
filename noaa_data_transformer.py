9a91a3015662d067a0e8b141d8938f51e4477121
jmerone <jmerone94@gmail.com>
2018-10-20 13:18:18 -0400

    code for noaa api

--- noaa_data_transformer.py
+++ noaa_data_transformer.py
@@ -1,9 +1,16 @@
 import json
-import cv2
-#from xml.dom import minidom
+import os
 #pip install noaa-sdk
 from noaa_sdk import noaa
+os.chdir( r"C:\Users\Jean\Documents\Python_Programming\eye-of-the-storm")
 n  = noaa.NOAA()
 
-#parameter is  zipcode, country
- n.get_observations('33432','US')
+observations = n.get_observations('33432','US',
+                                  start='2018-01-01',
+                                  end='2018-10-12')
+#boca raton zip code: all NOAA capture dates seem to be on May 14 2018
+with open("noaa_boca_2018.json", "a") as write_file:
+    for observation in observations:
+        json.dump(observation, write_file)
+        
+
